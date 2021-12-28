2592578a379ff2e635b2b6f78079d168cd75cf06
resurrectiontent <asvoskresenskii@gmail.com>
2021-12-28 19:53:44 +0300

    task 8 revise

--- labs/Assignment8.cpp
+++ labs/Assignment8.cpp
@@ -3,11 +3,24 @@
 #define FILL_VALUE 1
 #define SMOOTHING 10
 #define MIN_MESSAGE 1
-#define MAX_MESSAGE 1000000
+#define MAX_MESSAGE     1000000
+#define LOGGING_PERIOD  100000
 using namespace std;
 
 double avg(double *arr, int length){
     double sum = 0;
+    for (int i = 0; i < length; ++i)
+        sum += arr[i];
+    return sum / length;
+}
+float avg(float *arr, int length){
+    float sum = 0;
+    for (int i = 0; i < length; ++i)
+        sum += arr[i];
+    return sum / (float)length;
+}
+int avg(int *arr, int length){
+    int sum = 0;
     for (int i = 0; i < length; ++i)
         sum += arr[i];
     return sum / length;
@@ -25,6 +38,9 @@ int main(int argc, char* argv[]) {
     if (rank == 0){
         double startTime, finishTime;
         double *localTimesStorage = new double[SMOOTHING];
+        int *allBytes = new int[LOGGING_PERIOD];
+        double *allBWs = new double[LOGGING_PERIOD];
+        double *allLats = new double[LOGGING_PERIOD];
         int bestBytes = 0;
         float bestBW = .0;
         double bestLat = .0;
@@ -42,18 +58,28 @@ int main(int argc, char* argv[]) {
             double avgTime = avg(localTimesStorage, SMOOTHING);
             double bandwidth = 2 * bytes /  avgTime / 1024 / 1024;
 
+            allBytes[i % LOGGING_PERIOD] = bytes;
+            allBWs[i % LOGGING_PERIOD] = bandwidth;
+
             if (bandwidth > bestBW){
                 bestBW = (float)bandwidth;
                 bestBytes = bytes;
                 bestLat = avgTime / 2;
             }
-            if (i % 100000 == 1)
-                cout << "Best for i=" << i << ":\tlen: " << bestBytes << " B\t bandwidth: " << bestBW << " MB/s\t latency: " << bestLat << endl;
+            if (i == 1){
+                cout << "For the first measurement:\t\t" << (float)bytes / 1024 / 1024 << " MB \t" << bandwidth << " MB/s" << endl;
+            }
+            else if (i % LOGGING_PERIOD == 1)
+                cout << "Avg values for measurements " << i-LOGGING_PERIOD << "-" << i << ":\t"
+                    << (float)bytes / 1024 / 1024 << " MB \t" << bandwidth << " MB/s" << endl;
         }
 
-        cout << "len: " << bestBytes << " B\t bandwidth: " << bestBW << " MB/s\t latency: " << bestLat << endl;
+        cout << "The Best results:\t len: " << bestBytes << " B\t bandwidth: " << bestBW << " MB/s\t latency: " << bestLat << endl;
 
         delete[](localTimesStorage);
+        delete[](allBWs);
+        delete[](allLats);
+        delete[](allBytes);
     }
     else if(rank == 1)
         for(int i = MIN_MESSAGE; i <= MAX_MESSAGE; i += 10)
